d1c7523ae3d381e79e3aaafe2b8c279757e43198
Neil Roberts <bpeeluk@yahoo.co.uk>
2019-06-22 09:55:08 +0200

    Start working on a TTY test wrapper

---
+++ src/pcx-tty-game.c
@@ -0,0 +1,277 @@
+/*
+ * Puxcobot - A robot to play Coup in Esperanto (Puĉo)
+ * Copyright (C) 2019  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "pcx-tty-game.h"
+
+#include <assert.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#include "pcx-game.h"
+#include "pcx-util.h"
+#include "pcx-buffer.h"
+#include "pcx-main-context.h"
+
+struct pcx_error_domain
+pcx_tty_game_error;
+
+struct pcx_tty_game_player {
+        struct pcx_main_context_source *source;
+        struct pcx_buffer buffer;
+        int fd;
+};
+
+struct pcx_tty_game {
+        struct pcx_game *game;
+        int n_players;
+        struct pcx_tty_game_player players[PCX_GAME_MAX_PLAYERS];
+        struct pcx_buffer buffer;
+};
+
+static void
+send_message_to_fd(struct pcx_tty_game *game,
+                   int fd,
+                   enum pcx_game_message_format format,
+                   const char *message,
+                   size_t n_buttons,
+                   const struct pcx_game_button *buttons)
+{
+        pcx_buffer_set_length(&game->buffer, 0);
+        pcx_buffer_append_string(&game->buffer, message);
+        pcx_buffer_append_c(&game->buffer, '\n');
+
+        if (n_buttons > 0) {
+                pcx_buffer_append_c(&game->buffer, '\n');
+
+                for (unsigned i = 0; i < n_buttons; i++) {
+                        pcx_buffer_append_printf(&game->buffer,
+                                                 "%s) %s\n",
+                                                 buttons[i].data,
+                                                 buttons[i].text);
+                }
+        }
+
+        size_t wrote = 0;
+
+        while (wrote < game->buffer.length) {
+                ssize_t w = write(fd,
+                                  game->buffer.data + wrote,
+                                  game->buffer.length - wrote);
+                if (w <= 0)
+                        break;
+                wrote += w;
+        }
+}
+
+static void
+send_private_message_cb(int user_num,
+                        enum pcx_game_message_format format,
+                        const char *message,
+                        size_t n_buttons,
+                        const struct pcx_game_button *buttons,
+                        void *user_data)
+{
+        struct pcx_tty_game *game = user_data;
+
+        assert(user_num >= 0 && user_num < game->n_players);
+
+        send_message_to_fd(game,
+                           game->players[user_num].fd,
+                           format,
+                           message,
+                           n_buttons,
+                           buttons);
+}
+
+static void
+send_message_cb(const char *message,
+                enum pcx_game_message_format format,
+                size_t n_buttons,
+                const struct pcx_game_button *buttons,
+                void *user_data)
+{
+        struct pcx_tty_game *game = user_data;
+
+        send_message_to_fd(game,
+                           STDOUT_FILENO,
+                           format,
+                           message,
+                           n_buttons,
+                           buttons);
+}
+
+static const struct pcx_game_callbacks
+callbacks = {
+        .send_private_message = send_private_message_cb,
+        .send_message = send_message_cb,
+};
+
+static void
+poll_cb(struct pcx_main_context_source *source,
+        int fd,
+        enum pcx_main_context_poll_flags flags,
+        void *user_data)
+{
+        struct pcx_tty_game *game = user_data;
+        int player_num;
+
+        for (player_num = 0; player_num < game->n_players; player_num++) {
+                if (game->players[player_num].fd == fd)
+                        goto found_player_num;
+        }
+
+        pcx_fatal("Unexpected FD in poll_cb");
+
+found_player_num: (void) 0;
+
+        struct pcx_tty_game_player *player = game->players + player_num;
+
+        pcx_buffer_ensure_size(&player->buffer, player->buffer.length + 128);
+
+        ssize_t got = read(player->fd,
+                           player->buffer.data + player->buffer.length,
+                           player->buffer.size - player->buffer.length);
+
+        if (got <= 0) {
+                pcx_main_context_remove_source(player->source);
+                player->source = NULL;
+                return;
+        }
+
+        player->buffer.length += got;
+
+        size_t processed = 0;
+
+        while (true) {
+                uint8_t *end = memchr(player->buffer.data + processed,
+                                      '\n',
+                                      player->buffer.length - processed);
+                if (end == NULL)
+                        break;
+
+                uint8_t *data_end = end;
+
+                while (data_end > player->buffer.data + processed &&
+                       (data_end[-1] == '\r' || data_end[-1] == ' ')) {
+                        data_end--;
+                }
+
+                *data_end = '\0';
+
+                pcx_game_handle_callback_data(game->game,
+                                              player_num,
+                                              (const char *)
+                                              player->buffer.data + processed);
+
+                processed = end - player->buffer.data + 1;
+        }
+
+        memmove(player->buffer.data,
+                player->buffer.data + processed,
+                player->buffer.length - processed);
+        player->buffer.length -= processed;
+}
+
+static const char *
+get_basename(const char *filename)
+{
+        const char *bn = filename + strlen(filename);
+
+        while (bn > filename && bn[-1] != '/' && bn[-1] != '\\')
+                bn--;
+
+        return bn;
+}
+
+struct pcx_tty_game *
+pcx_tty_game_new(int n_players,
+                 const char * const *files,
+                 struct pcx_error **error)
+{
+        assert(n_players > 0 && n_players <= PCX_GAME_MAX_PLAYERS);
+
+        struct pcx_tty_game *game = pcx_calloc(sizeof *game);
+
+        pcx_buffer_init(&game->buffer);
+
+        game->n_players = n_players;
+
+        for (unsigned i = 0; i < PCX_N_ELEMENTS(game->players); i++) {
+                game->players[i].fd = -1;
+                pcx_buffer_init(&game->players[i].buffer);
+        }
+
+        for (unsigned i = 0; i < n_players; i++) {
+                game->players[i].fd = open(files[i], O_RDWR);
+                if (game->players[i].fd == -1) {
+                        pcx_set_error(error,
+                                      &pcx_tty_game_error,
+                                      PCX_TTY_GAME_ERROR_IO,
+                                      "Error opening TTY: %s: %s",
+                                      files[i],
+                                      strerror(errno));
+                        goto error;
+                }
+
+                game->players[i].source =
+                        pcx_main_context_add_poll(NULL,
+                                                  game->players[i].fd,
+                                                  PCX_MAIN_CONTEXT_POLL_IN |
+                                                  PCX_MAIN_CONTEXT_POLL_ERROR,
+                                                  poll_cb,
+                                                  game);
+        }
+
+        const char *names[PCX_GAME_MAX_PLAYERS];
+
+        for (unsigned i = 0; i < n_players; i++)
+                names[i] = get_basename(files[i]);
+
+        game->game = pcx_game_new(&callbacks,
+                                  game,
+                                  n_players,
+                                  names);
+
+        return game;
+
+error:
+        pcx_tty_game_free(game);
+        return NULL;
+}
+
+void
+pcx_tty_game_free(struct pcx_tty_game *game)
+{
+        if (game->game)
+                pcx_game_free(game->game);
+
+        for (unsigned i = 0; i < PCX_N_ELEMENTS(game->players); i++) {
+                if (game->players[i].fd != -1)
+                        pcx_close(game->players[i].fd);
+                if (game->players[i].source)
+                        pcx_main_context_remove_source(game->players[i].source);
+                pcx_buffer_destroy(&game->players[i].buffer);
+        }
+
+        pcx_buffer_destroy(&game->buffer);
+
+        pcx_free(game);
+}
