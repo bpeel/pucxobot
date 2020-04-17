0b92b89e2dccdf919d9124c99bbbc4a75b7db71f
Neil Roberts <bpeeluk@yahoo.co.uk>
2020-04-17 20:23:02 +0200

    server: Make a dummy server that parses JSON and echos it

--- src/pcx-server.c
+++ src/pcx-server.c
@@ -43,6 +43,11 @@
 struct pcx_error_domain
 pcx_server_error;
 
+struct pcx_server_response {
+        struct pcx_list link;
+        struct json_object *object;
+};
+
 struct pcx_server_connection {
         struct pcx_list link;
 
@@ -65,8 +70,24 @@ struct pcx_server_connection {
          * we've had bad input and we're ignoring further data
          */
         bool write_finished;
+        /* Set to true if we’ve sent some data to the tokener without
+         * receiving a complete object. In that case it is invalid for
+         * the client to close their writing end.
+         */
+        bool partial_object;
 
-        struct pcx_buffer buf;
+        /* Queue of pcx_server_response to write to the connection */
+        struct pcx_list response_queue;
+        /* How much data is in write_buf */
+        size_t output_length;
+        /* How much of the first response has already been copied into
+         * write_buf
+         */
+        size_t partial_response_written;
+
+        uint8_t write_buf[1024];
+
+        struct json_tokener *tokener;
 };
 
 struct pcx_server {
@@ -80,6 +101,9 @@ struct pcx_server {
         bool socket_bound;
 
         struct pcx_list connections;
+
+        /* Temporary buffer for reading data to feed into the tokener */
+        char read_buf[1024];
 };
 
 static bool
@@ -103,14 +127,36 @@ set_nonblock(int sock,
 }
 
 static void
+remove_response(struct pcx_server_response *response)
+{
+        json_object_put(response->object);
+        pcx_list_remove(&response->link);
+        pcx_free(response);
+}
+
+static void
+remove_responses(struct pcx_server_connection *connection)
+{
+        struct pcx_server_response *response, *tmp;
+
+        pcx_list_for_each_safe(response, tmp,
+                               &connection->response_queue,
+                               link) {
+                remove_response(response);
+        }
+}
+
+static void
 remove_connection(struct pcx_server_connection *connection)
 {
+        remove_responses(connection);
+
+        if (connection->tokener)
+                json_tokener_free(connection->tokener);
         if (connection->source)
                 pcx_main_context_remove_source(connection->source);
         if (connection->sock != -1)
                 close(connection->sock);
-
-        pcx_buffer_destroy(&connection->buf);
 
         pcx_list_remove(&connection->link);
 
@@ -128,7 +174,8 @@ update_poll(struct pcx_server_connection *connection)
         /* Shutdown the socket if we've finished writing */
         if (!connection->write_finished &&
             (connection->read_finished || connection->had_bad_input) &&
-            connection->buf.length == 0) {
+            pcx_list_empty(&connection->response_queue) &&
+            connection->output_length == 0) {
                 if (shutdown(connection->sock, SHUT_WR) == -1) {
                         remove_connection(connection);
                         return;
@@ -146,11 +193,142 @@ update_poll(struct pcx_server_connection *connection)
         }
 
         if (!connection->write_finished) {
-                if (connection->buf.length > 0)
+                if (connection->output_length > 0 ||
+                    !pcx_list_empty(&connection->response_queue))
                         flags |= PCX_MAIN_CONTEXT_POLL_OUT;
         }
 
         pcx_main_context_modify_poll(connection->source, flags);
+}
+
+static void
+queue_response(struct pcx_server_connection *connection,
+               struct json_object *obj)
+{
+        struct pcx_server_response *response = pcx_alloc(sizeof *response);
+
+        response->object = json_object_get(obj);
+
+        pcx_list_insert(connection->response_queue.prev, &response->link);
+}
+
+static void
+set_bad_input(struct pcx_server_connection *connection)
+{
+        struct pcx_server_response *partial_response = NULL;
+
+        /* If we are part way through sending a response then keep it */
+        if (connection->partial_response_written) {
+                partial_response =
+                        pcx_container_of(connection->response_queue.next,
+                                         struct pcx_server_response,
+                                         link);
+                pcx_list_remove(&partial_response->link);
+        }
+
+        /* Remove all other responses */
+        remove_responses(connection);
+
+        /* Add the first one back */
+        if (partial_response) {
+                pcx_list_insert(&connection->response_queue,
+                                &partial_response->link);
+        }
+
+        json_object *str = json_object_new_string("Invalid request");
+        json_object *obj = json_object_new_object();
+        json_object_object_add(obj, "error", str);
+
+        queue_response(connection, obj);
+
+        json_object_put(obj);
+
+        connection->had_bad_input = true;
+}
+
+static void
+handle_request(struct pcx_server_connection *connection,
+               struct json_object *object,
+               bool *need_update)
+{
+        /* Stub handler just queues the request as a response */
+        queue_response(connection, object);
+
+        *need_update = true;
+}
+
+static void
+parse_json_objects(struct pcx_server_connection *connection,
+                   size_t buf_size,
+                   const char *buf,
+                   bool *need_update)
+{
+        while (true) {
+                struct json_object *object =
+                        json_tokener_parse_ex(connection->tokener,
+                                              buf,
+                                              buf_size);
+
+                if (object == NULL) {
+                        enum json_tokener_error error =
+                                json_tokener_get_error(connection->tokener);
+
+                        if (error == json_tokener_continue) {
+                                connection->partial_object = true;
+                        } else {
+                                set_bad_input(connection);
+                                *need_update = true;
+                        }
+                        break;
+                }
+
+                buf_size -= connection->tokener->char_offset;
+                buf += connection->tokener->char_offset;
+
+                handle_request(connection, object, need_update);
+
+                json_object_put(object);
+
+                json_tokener_reset(connection->tokener);
+
+                if (buf_size <= 0) {
+                        connection->partial_object = false;
+                        break;
+                }
+        }
+}
+
+static void
+fill_write_buf(struct pcx_server_connection *connection)
+{
+        while (connection->output_length < sizeof connection->write_buf &&
+               !pcx_list_empty(&connection->response_queue)) {
+                struct pcx_server_response *response =
+                        pcx_container_of(connection->response_queue.next,
+                                         struct pcx_server_response,
+                                         link);
+
+                const char *str = json_object_to_json_string(response->object);
+                size_t len = strlen(str);
+                size_t to_write =
+                        MIN((sizeof connection->write_buf) -
+                            connection->output_length,
+                            len - connection->partial_response_written);
+
+                memcpy(connection->write_buf +
+                       connection->output_length,
+                       str + connection->partial_response_written,
+                       to_write);
+
+                if (to_write + connection->partial_response_written >= len) {
+                        connection->partial_response_written = 0;
+                        remove_response(response);
+                } else {
+                        connection->partial_response_written += to_write;
+                }
+
+                connection->output_length += to_write;
+        }
 }
 
 static void
@@ -160,6 +338,7 @@ connection_cb(struct pcx_main_context_source *source,
               void *user_data)
 {
         struct pcx_server_connection *connection = user_data;
+        struct pcx_server *server = connection->server;
         bool need_update = false;
 
         if ((flags & PCX_MAIN_CONTEXT_POLL_ERROR)) {
@@ -168,12 +347,9 @@ connection_cb(struct pcx_main_context_source *source,
         }
 
         if ((flags & PCX_MAIN_CONTEXT_POLL_IN)) {
-                pcx_buffer_ensure_size(&connection->buf,
-                                       connection->buf.length + 512);
-
                 int got = read(fd,
-                               connection->buf.data,
-                               connection->buf.size - connection->buf.length);
+                               server->read_buf,
+                               sizeof server->read_buf);
 
                 if (got == -1) {
                         if (errno != EWOULDBLOCK && errno != EAGAIN) {
@@ -182,18 +358,26 @@ connection_cb(struct pcx_main_context_source *source,
                         }
                 } else if (got == 0) {
                         connection->read_finished = true;
+                        if (!connection->had_bad_input &&
+                            connection->partial_object)
+                                set_bad_input(connection);
                         need_update = true;
                 } else {
-                        if (connection->buf.length == 0)
-                                need_update = true;
-                        connection->buf.length += got;
+                        if (!connection->had_bad_input) {
+                                parse_json_objects(connection,
+                                                   got,
+                                                   server->read_buf,
+                                                   &need_update);
+                        }
                 }
         }
 
         if ((flags & PCX_MAIN_CONTEXT_POLL_OUT)) {
+                fill_write_buf(connection);
+
                 int wrote = write(fd,
-                                  connection->buf.data,
-                                  connection->buf.length);
+                                  connection->write_buf,
+                                  connection->output_length);
 
                 if (wrote == -1) {
                         if (errno != EWOULDBLOCK && errno != EAGAIN) {
@@ -201,10 +385,10 @@ connection_cb(struct pcx_main_context_source *source,
                                 return;
                         }
                 } else {
-                        memmove(connection->buf.data,
-                                connection->buf.data + wrote,
-                                connection->buf.length - wrote);
-                        connection->buf.length -= wrote;
+                        memmove(connection->write_buf,
+                                connection->write_buf + wrote,
+                                connection->output_length - wrote);
+                        connection->output_length -= wrote;
 
                         need_update = true;
                 }
@@ -266,7 +450,8 @@ server_socket_cb(struct pcx_main_context_source *source,
 
         connection->server = server;
         connection->sock = client_fd;
-        pcx_buffer_init(&connection->buf);
+        connection->tokener = json_tokener_new();
+        pcx_list_init(&connection->response_queue);
         connection->source =
                 pcx_main_context_add_poll(NULL,
                                           client_fd,
