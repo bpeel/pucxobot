5c65ca163b3c6a1e8483998c4baaef2cbb1f3468
Neil Roberts <bpeeluk@yahoo.co.uk>
2022-04-15 10:31:09 +0200

    Add a trie structure that can be added to
    This will later be used to detect duplicate words in Wordparty.

---
+++ src/pcx-trie.c
@@ -0,0 +1,108 @@
+/*
+ * Puxcobot - A robot to play Coup in Esperanto (Puĉo)
+ * Copyright (C) 2022  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "pcx-trie.h"
+
+#include <stdalign.h>
+#include <stdint.h>
+
+#include "pcx-slab.h"
+#include "pcx-buffer.h"
+#include "pcx-utf8.h"
+
+struct pcx_trie_node {
+        uint32_t ch;
+        struct pcx_trie_node *next_sibling;
+        struct pcx_trie_node *first_child;
+};
+
+struct pcx_trie {
+        struct pcx_slab_allocator node_allocator;
+        struct pcx_trie_node *root;
+};
+
+static struct pcx_trie_node *
+allocate_node(struct pcx_trie *trie,
+              uint32_t ch)
+{
+        struct pcx_trie_node *node =
+                pcx_slab_allocate(&trie->node_allocator,
+                                  sizeof (struct pcx_trie_node),
+                                  alignof (struct pcx_trie_node));
+
+        node->ch = ch;
+        node->next_sibling = NULL;
+        node->first_child = NULL;
+
+        return node;
+}
+
+struct pcx_trie *
+pcx_trie_new(void)
+{
+        struct pcx_trie *trie = pcx_alloc(sizeof *trie);
+
+        pcx_slab_init(&trie->node_allocator);
+
+        trie->root = NULL;
+
+        return trie;
+}
+
+enum pcx_trie_add_result
+pcx_trie_add_word(struct pcx_trie *trie,
+                  const char *word)
+{
+        struct pcx_trie_node *node = trie->root;
+        struct pcx_trie_node **parent_slot = &trie->root;
+
+        while (true) {
+                uint32_t ch = pcx_utf8_get_char(word);
+
+                if (node == NULL) {
+                        struct pcx_trie_node *child = allocate_node(trie, ch);
+                        *parent_slot = child;
+                        parent_slot = &child->first_child;
+
+                        if (ch == '\0')
+                                return PCX_TRIE_ADD_RESULT_NEW_WORD;
+
+                        word = pcx_utf8_next(word);
+                } else if (ch == node->ch) {
+                        parent_slot = &node->first_child;
+                        node = node->first_child;
+
+                        if (ch == '\0')
+                                return PCX_TRIE_ADD_RESULT_ALREADY_ADDED;
+
+                        word = pcx_utf8_next(word);
+                } else {
+                        parent_slot = &node->next_sibling;
+                        node = node->next_sibling;
+                }
+        }
+}
+
+void
+pcx_trie_free(struct pcx_trie *trie)
+{
+        pcx_slab_destroy(&trie->node_allocator);
+        pcx_free(trie);
+}
---
+++ src/test-trie.c
@@ -0,0 +1,154 @@
+/*
+ * Puxcobot - A robot to play Coup in Esperanto (Puĉo)
+ * Copyright (C) 2022  Neil Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config.h"
+
+#include "pcx-trie.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "pcx-buffer.h"
+#include "pcx-util.h"
+
+#define MAX_LETTERS_IN_WORD 4
+#define N_BITS_PER_LETTER 3
+
+static const char * const
+letters[] = {
+        "a", "b", "c", "d", "🎩", "Ŝ", "É", "🔚",
+};
+
+_Static_assert(PCX_N_ELEMENTS(letters) == (1 << N_BITS_PER_LETTER),
+               "There must be one letter string for each possible letter "
+               "number");
+
+static int
+get_n_words(void)
+{
+        int n_words_for_length = 1 << N_BITS_PER_LETTER;
+        int total_n_words = 0;
+
+        for (int n_letters = 1; n_letters <= MAX_LETTERS_IN_WORD; n_letters++) {
+                total_n_words += n_words_for_length;
+                n_words_for_length <<= N_BITS_PER_LETTER;
+        }
+
+        return total_n_words;
+}
+
+static int
+get_word_length(uint32_t word_num)
+{
+        int n_words_for_length = 1 << N_BITS_PER_LETTER;
+        int total_n_words = 0;
+
+        for (int n_letters = 1; n_letters <= MAX_LETTERS_IN_WORD; n_letters++) {
+                total_n_words += n_words_for_length;
+
+                if (word_num < total_n_words)
+                        return n_letters;
+
+                n_words_for_length <<= N_BITS_PER_LETTER;
+        }
+
+        assert(!"unknown word_num");
+}
+
+static void
+get_word(struct pcx_buffer *buf,
+         uint32_t word_num)
+{
+        /* Invert some bits so that the letters aren’t added in order */
+        word_num ^= 5;
+
+        pcx_buffer_set_length(buf, 0);
+
+        int length = get_word_length(word_num);
+
+        for (int i = 0; i < length; i++) {
+                int letter_num = word_num & ((1 << N_BITS_PER_LETTER) - 1);
+                pcx_buffer_append_string(buf, letters[letter_num]);
+                word_num >>= N_BITS_PER_LETTER;
+        }
+}
+
+static bool
+check_all_added(struct pcx_trie *trie,
+                uint32_t n_words,
+                struct pcx_buffer *word_buf)
+{
+        for (uint32_t word_num = 0; word_num < n_words; word_num++) {
+                get_word(word_buf, word_num);
+
+                enum pcx_trie_add_result result =
+                        pcx_trie_add_word(trie, (const char *) word_buf->data);
+
+                if (result != PCX_TRIE_ADD_RESULT_ALREADY_ADDED) {
+                        fprintf(stderr,
+                                "When readding “%s” (%i) result is not "
+                                "ALREADY_ADDED\n",
+                                (const char *) word_buf->data,
+                                word_num);
+                        return false;
+                }
+        }
+
+        return true;
+}
+
+int
+main(int argc, char **argv)
+{
+        int ret = EXIT_SUCCESS;
+
+        struct pcx_trie *trie = pcx_trie_new();
+
+        struct pcx_buffer word_buf = PCX_BUFFER_STATIC_INIT;
+
+        int n_words = get_n_words();
+
+        for (uint32_t word_num = 0; word_num < n_words; word_num++) {
+                if (!check_all_added(trie, word_num, &word_buf)) {
+                        ret = EXIT_FAILURE;
+                        break;
+                }
+
+                get_word(&word_buf, word_num);
+
+                enum pcx_trie_add_result result =
+                        pcx_trie_add_word(trie,
+                                          (const char *) word_buf.data);
+
+                if (result != PCX_TRIE_ADD_RESULT_NEW_WORD) {
+                        fprintf(stderr,
+                                "Added “%s” (%i) but result was not "
+                                "PCX_TRIE_ADD_RESULT_NEW_WORD\n",
+                                (const char *) word_buf.data,
+                                word_num);
+                        ret = EXIT_FAILURE;
+                        break;
+                }
+        }
+
+        pcx_buffer_destroy(&word_buf);
+        pcx_trie_free(trie);
+
+        return ret;
+}
